84ff6f7ef354e139ef828bda38eec592d1104e66
songkang <songkangvip@163.com>
2015-10-16 14:42:15 +0800

    Add some Source code

---
+++ src/com/fhalo/activities/MainActivity.java
@@ -0,0 +1,39 @@
+
+ /**
+ * @classDescription:
+ * @projectName: FhaloLauncher
+ * @packageName: com.fhalo.activities
+ * @className: MainActivity
+ * @author: SouKou
+ * @date: 2015年10月16日下午2:03:26
+ * @copyright: 北京天与华符科技有限公司
+ * @mail:
+ */
+
+package com.fhalo.activities;
+
+import com.fhalo.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * @title: MainActivity.java
+ * @packageName: com.fhalo.activities
+ * @description: TODO
+ * @author: SouKou
+ * @date: 2015年10月16日 下午2:03:26
+ * @modifier: SouKou
+ * @modifyTime: 2015年10月16日下午2:03:26
+ * @modifyNote:
+ * @version: v1.0
+ */
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.main);
+	}
+}
